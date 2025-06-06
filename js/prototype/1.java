c418889ce2a48edef445bb412fdb4a919b58aa36
Acscanf <1657170980@qq.com>
2025-06-06 17:12:07 +0800

    'JS中的原型机制'

---
+++ js/prototype/1.java
@@ -0,0 +1,13 @@
+// 定义Puppy类
+public class Puppy {
+    int puppyAge;
+    // 构造函数
+    public Puppy(age) {
+        // This constructor has one parameter, name.
+        puppyAge = age;
+    }
+    // 公有方法
+    public void say() {
+        System.out.println("汪汪汪");
+    }
+}
