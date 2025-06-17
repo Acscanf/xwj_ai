ee9d69112e35d0f5fb118a9fd1cab6b26365b527
Acscanf <1657170980@qq.com>
2025-06-17 23:21:21 +0800

    '修改也界面添加效果'

--- html5/localStorage/common.js
+++ html5/localStorage/common.js
@@ -1,9 +1,33 @@
-const addItems = document.querySelector('.add-items');  //form
-const itemsList = document.querySelector('.plates');  // 列表
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+let items = JSON.parse(localStorage.getItem('tapasItems')) || [];
 
 function addItem(e) {
-    e.preventDefault(); // 阻止表单提交
-    
+  e.preventDefault();
+  const text = this.querySelector('[name=item]').value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('tapasItems', JSON.stringify(items));
+  this.reset();
 }
 
-addItems.addEventListener('submit' , addItem);
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}>
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+// 初始化加载
+populateList(items, itemsList);
