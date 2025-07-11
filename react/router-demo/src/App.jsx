0e6c52475078679750f4adfc242754479f2bdfd1
Acscanf <1657170980@qq.com>
2025-07-11 21:32:15 +0800

    'react-router-dom的路由基础学习'

---
+++ react/router-demo/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { 
+  BrowserRouter as Router,  // 前端路由
+  Routes,  // 路由设置容器
+  Route,  // 单条路由
+} from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'  // 引入页面组件
+import About from './pages/About'  // 引入页面组件
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      前端路由接管一切
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ react/router-demo/src/pages/About/index.jsx
@@ -0,0 +1,9 @@
+const About = function() {
+    return (
+        <>
+            About
+        </>
+    )
+}
+
+export default About
