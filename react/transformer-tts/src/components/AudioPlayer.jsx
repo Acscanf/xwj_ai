6b4f3240215b04660f5a5708f3675f23b00f32de
Acscanf <1657170980@qq.com>
2025-08-11 19:35:44 +0800

    '更加深入的了解了对应的代码编写'

--- react/transformer-tts/src/components/AudioPlayer.jsx
+++ react/transformer-tts/src/components/AudioPlayer.jsx
@@ -1,31 +1,32 @@
-import {
-    useEffect,
-    useRef
-} from 'react'
+import { useEffect, useRef } from 'react';
 
 const AudioPlayer = ({ audioUrl, mimeType }) => {
-    const audioPlayer = useRef(null)
+
+    const audioPlayer = useRef(null);
     const audioSource = useRef(null);
 
     useEffect(() => {
         if (audioPlayer.current && audioSource.current) {
             audioSource.current.src = audioUrl;
-            audioPlayer.current.play();
+            //audioPlayer.current.play()
+            //在 useEffect 里调用了 audioPlayer.current.play()，页面还没用户交互就尝试播放 → 被拦截。
+
         }
     }, [audioUrl])
+
     return (
-        <div className="flex relative z-10 my-4 w-full">
-            <audio
-                ref={audioPlayer}
-                controls
-                className="w-full h-14 rounded-lg bg-white 
-                shadow-xl shadow-black/5 ring-1 ring-slate-700/10"
-            >
-                {/* 浏览器兼容声音的类型不一样 多个source */}
-                <source ref={audioSource} type={mimeType} />
-            </audio>
-        </div>
+        <>
+            <div className="flex relative z-10 my-4 w-full">
+                <audio
+                    ref={audioPlayer}
+                    controls
+                    className="w-full h-14 rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10">
+                    {/* 浏览器兼容的声音的类型不一样，所以可能有多个source */}
+                    <source ref={audioSource} type={mimeType} />
+                </audio>
+            </div>
+        </>
     )
 }
 
-export default AudioPlayer
+export default AudioPlayer;
--- react/transformer-tts/src/worker.js
+++ react/transformer-tts/src/worker.js
@@ -1,8 +1,46 @@
 import {
-    env
+    env, // 配置AI模型运行环境
+    Tensor, // AI 大模型处理数据的基本单位
+    AutoTokenizer, // AI 自行分词器
+    SpeechT5ForTextToSpeech, // 文本转语音模型  语音的特征
+    SpeechT5HifiGan, // 语音合成模型  和音色合成
 } from '@xenova/transformers'
-
+// huggingFace 开源的大模型社区
+// 禁用本地大模型，去请求远程的 tts 模型
+env.allowLocalModels = false // 禁止本地模型
 // transformer.js  文本 -> 语音 tts
-self.onmessage = (e) => {
-    console.log(e)
+// 单例模式  核心难点
+// 多次执行tts ai 业务，但是只会实例化一次
+class MyTextToSpeechPipeline {
+    // AI 语音模型的数据源地址，用于下载不同说话人的声音特征向量
+    // 每个字，每个词
+    static BASE_URL = 'https://huggingface.co/datasets/Xenova/cmu-arctic-xvectors-extracted/resolve/main/';
+    // 文本 -> speecht5_tts 语音特征
+    static model_id = "Xenova/speecht5_tts"; // 模型的ID，用于指定要加载的模型
+    // 语言特征 -> sppecht5_hifigan  -> 特有的角色音频文件
+    static vocoder_id = "/sppecht5_hifigan"; // 语音合成模型的ID，用于指定要加载的语音合成模型
+    // 分词器实例
+    static tokenizer_id = null;
+    // 模型实例
+    static model_instance = null; 
+    // 合成实例
+    static vocoder_instance = null; 
+    static async getInstance(progress_callback = null) {
+        if(this.tokenizer_id === null) {
+            // 之前处理过的大模型，被预先训练过的
+            this.tokenizer_id = AutoTokenizer.from_pretrained(this.model_id, {
+                progress_callback: progress_callback, // 进度条
+            })
+        }
+    }
+}
+
+
+self.onmessage = async (e) => {
+    // console.log(e)
+    // ai pipeline 派发一个nlp任务
+    // 懒加载  llm 初始化和加载放到第一次任务抵用之时
+    const [] = await MyTextToSpeechPipeline.getInstance(x => {
+        self.postMessage(x)
+    })
 }
