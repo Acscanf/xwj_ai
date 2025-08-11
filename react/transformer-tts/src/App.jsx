b316030e1362cbbaa1e07f2180382c39531883da
Acscanf <1657170980@qq.com>
2025-08-11 22:52:00 +0800

    '完成了基本的项目功能'

--- react/transformer-tts/src/App.jsx
+++ react/transformer-tts/src/App.jsx
@@ -1,12 +1,12 @@
-import {
+import { 
   useState,
   useRef,
   useEffect
 } from 'react'
-import Progess from './components/Progress';
+import Progress from './components/Progress';
 import AudioPlayer from './components/AudioPlayer';
 import {
-  SPEAKERS,
+  SPEAKERS, 
   DEFAULT_SPEAKER
 } from './constants'
 
@@ -31,16 +31,42 @@ function App() {
     worker.current = new Worker(new URL('./worker.js', import.meta.url), {
       type: 'module'
     })
-    worker.current.postMessage({
-      text: '灵不灵，奔驰s680'
-    })
 
-    const onMessageReceived = () => {
-
+    const onMessageReceived = (e) => {
+      console.log(e, '来自主线程');
+      switch(e.data.status) {
+        case 'initiate':
+          // llm ready 了吗？
+          setReady(false);
+          setProgressItems(prev => [...prev, e.data])
+        break;
+        case 'progress':
+          // console.log(e.data)
+          setProgressItems(
+            prev => prev.map(item => {
+              if (item.file === e.data.file) {
+                return {
+                  ...item,
+                  progress: e.data.progress
+                }
+              }
+              return item
+            })
+          )
+        break;
+        case 'done':
+        setProgressItems(
+          prev => prev.filter(item => item.file !== e.data.file)
+        )
+        break;
+        case 'ready':
+          setReady(true);
+        break;
+      }
     }
     worker.current.onmessage = onMessageReceived;
 
-    return () => worker.current.removeEventListener('message',
+    return () => worker.current.removeEventListener('message', 
       onMessageReceived
     )
   }, [])
@@ -52,8 +78,41 @@ function App() {
       speaker_id: selectedSpeaker
     });
   }
+
+  const isLoading = ready === false;
+
   return (
     <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      {/* llm 初始化 */}
+      <div 
+      className="absolute z-50 top-0 left-0 w-full h-full transition-all 
+      px-8 flex flex-col justify-center text-center"
+      style={{
+        opacity: isLoading? 1 : 0,
+        pointerEvents: isLoading? 'all': 'none',
+        background: 'rgba(0,0,0,0.9)',
+        backdropFilter: 'blur(8px)'
+      }}
+      >
+        {
+          isLoading && (
+            <label className="text-white text-xl p-3">
+              Loading models... (only run once)
+            </label>
+          )
+        }
+        {
+          progressItems.map(data => (
+            <div key={`${data.name}/${data.file}`}>
+              <Progress 
+                text={`${data.name}/${data.file}`} 
+                percentage={data.progress}
+              /> 
+            </div>
+          ))
+        }
+      </div>
+      {/* tts 功能区 */}
       <div className="bg-white p-8 rounded-lg w-full max-w-xl m-2">
         <h1 className="text-3xl font-semibold text-gray-800 mb-1 text-center">
           In browser Text To Speech(端模型)
@@ -75,41 +134,41 @@ function App() {
           </textarea>
         </div>
         <div className="mb-4">
-          <label
-            htmlFor="speaker"
+          <label 
+            htmlFor="speaker" 
             className="block text-sm font-medium text-gray-600"
           >
           </label>
-          <select
+          <select 
             id="speaker"
             className="border border-gray-300 rounded-md p-2 w-full"
             value={selectedSpeaker}
-            onChange={(e) => setSelectedSpeaker(e.target.value)}
+            onChange={(e)=>setSelectedSpeaker(e.target.value)}
           >
-            {
-              // 可迭代对象快速转换成数组 [[key:val],[key1:value1],[key2:value1]]
-              Object.entries(SPEAKERS).map(([key, value]) => (
-                <option key={key} value={value}>
-                  {key}
-                </option>
-              ))
-            }
+          {
+            // 可迭代对象快速转换成数组 [[key:val],[key1:value1],[key2:value1]]
+            Object.entries(SPEAKERS).map(([key, value]) => (
+              <option key={key} value={value}>
+                {key}
+              </option>
+            ))
+          }
           </select>
         </div>
         <div className="flex justify-center">
           <button
-            className={`${disabled ?
-              'bg-gray-400 cursor-not-allowed' :
+            className={`${disabled? 
+              'bg-gray-400 cursor-not-allowed':
               'bg-blue-500 hover-bg-blue-600'
-              } text-white rouned-md py-2 px-4`}
+            } text-white rouned-md py-2 px-4`}
             onClick={handleGenerateSpeech}
             disabled={disabled}
           >
-            {disabled ? 'Generating...' : 'Generate'}
+            {disabled? 'Generating...': 'Generate'}
           </button>
         </div>
         {
-          output && <AudioPlayer
+          output && <AudioPlayer 
             audioUrl={output}
             mimeType={"audio/wav"}
           />
